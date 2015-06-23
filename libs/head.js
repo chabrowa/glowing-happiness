e2c71c6205ac4c6ae02b1eee7e9eb2e90b1bc349
chabrowa <chabrowa@leniak>
2015-06-23 12:54:58 +0100

    head pictures

---
+++ libs/head.js
@@ -0,0 +1,62 @@
+var headBase = [
+  {
+    name: 'head 1',
+    characteristics: [{name: 'health', value: 5}, {name: 'defence', value: 1}],
+    level: 1,
+    image: '/images/heads/head1.svg'
+  },
+  {
+    name: 'head 2',
+    characteristics: [{name: 'health', value: 5}, {name: 'luck', value: 5}],
+    level: 1,
+    image: '/images/heads/head2.svg'
+  },
+  {
+    name: 'head 3',
+    characteristics: [{name: 'defence', value: 1}, {name: 'luck', value: 5}],
+    level: 1,
+    image: '/images/heads/head3.svg'
+  },
+  {
+    name: 'head 4',
+    characteristics: [{name: 'defence', value: 3}, {name: 'luck', value: -5}],
+    level: 1,
+    image: '/images/heads/head4.svg'
+  },
+  {
+    name: 'head 5',
+    characteristics: [{name: 'luck', value: 10}],
+    level: 1,
+    image: '/images/heads/head5.svg'
+  },
+  {
+    name: 'head 6',
+    characteristics: [{name: 'health', value: 10}],
+    level: 1,
+    image: '/images/heads/head6.svg'
+  },
+  {
+    name: 'head 7',
+    characteristics: [{name: 'health', value: 10}, {name: 'defence', value: 1}, {name: 'luck', value: -5}],
+    level: 1,
+    image: '/images/heads/head7.svg'
+  },
+      {
+    name: 'head 8',
+    characteristics: [{name: 'defence', value: 2}],
+    level: 1,
+    image: '/images/heads/head8.svg'
+  },
+  {
+    name: 'head 9',
+    characteristics: [{name: 'health', value: 2}, {name: 'defence', value: 1}, {name: 'luck', value: 3}],
+    level: 1,
+    image: '/images/heads/head9.svg'
+  },
+  {
+    name: 'head 10',
+    characteristics: [{name: 'health', value: 3}, {name: 'defence', value: 1}, {name: 'luck', value: 2}],
+    level: 1,
+    image: '/images/heads/head10.svg'
+  },
+];

96ca2b72c1f020b03c8d5d5c6dde7e5d7946c0b8
chabrowa <chabrowa@leniak>
2015-06-23 13:52:42 +0100

    armor items

---
+++ libs/armor.js
@@ -0,0 +1,62 @@
+var armorBase = [
+  {
+    name: 'armor 1',
+    characteristics: [],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+  {
+    name: 'armor 2',
+    characteristics: [],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+  {
+    name: 'armor 3',
+    characteristics: [],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+  {
+    name: 'armor 4',
+    characteristics: [],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+  {
+    name: 'armor 5',
+    characteristics: [],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+  {
+    name: 'armor 6',
+    characteristics: [{name: 'health', value: 10}],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+  {
+    name: 'armor 7',
+    characteristics: [],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+      {
+    name: 'armor 8',
+    characteristics: [{name: 'defence', value: 2}],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+  {
+    name: 'armor 9',
+    characteristics: [],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+  {
+    name: 'armor 10',
+    characteristics: [],
+    level: 1,
+    image: '/images/armor/armor.svg'
+  },
+];
